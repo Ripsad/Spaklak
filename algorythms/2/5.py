5c3adb800f677d0eefb1421b52713115589096cf
Spaklak Laptop <kru4aew.egor@yandex.ru>
2023-10-17 11:01:43 +0300

    remade 5

--- algorythms/2/5.py
+++ algorythms/2/5.py
@@ -1,7 +1,9 @@
-a = int(input('Введите начало отрезка '))
-b = int(input('введите конец отрезка '))
-for i in range(a,b+1):
-    solve = []
-    for x in range(i):
-        solve.append(x)
-    print(f'{i}: {set(solve)}')
+import random
+solve = []
+random_first = random.randint(10000, 99999)
+solve.append(random_first)
+for i in range(9):
+    random_first = int(str(random_first**2)[2:7]) # диапозон получаемых значени 9 - 10, поэтому мы +- так берем серидину
+    solve.append(random_first)
+
+print(f'Output: {solve}')
